e91a16b87d9776d82a0f3bf1c6973893c3f69bb6
CesareTonola <c.tonola001@unibs.it>
2024-08-30 18:14:31 +0200

    fix

--- launch/bt_executer_kuka.launch.py
+++ launch/bt_executer_kuka.launch.py
@@ -53,7 +53,7 @@ def generate_launch_description():
            package="tf2_ros",
            executable="static_transform_publisher",
            name="foo_static_tf",
-           arguments=["0","0","-0.01","0","0","0","kuka_flange","foo_location"],
+           arguments=["0","0","-0.05","0","0","0","kuka_flange","foo_location"],
            output="screen")
       ]
     ),
